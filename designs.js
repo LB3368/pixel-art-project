95d900e4fca7831c55457d90b83f93dbf0f500c5
Latisha Bridges <latisha2801@gmail.com>
2022-07-26 22:04:32 -0400

    modified:   Styles.css Made changes to styles 	modified:   designs.js Made changes to designs 	modified:   index.html modified html

--- designs.js
+++ designs.js
@@ -2,12 +2,55 @@
 // Select size input
 var height, width, color;
 // When size is submitted by the user, call makeGrid()
+$(document).ready(function() {
+    $('sizePicker').submit(function makeGrid(grid) {
+        $('table').remove();
+        var rows = $('#input_height').val();
+        var cols = $('#input_width').val();
+            for (var i = 1; i <= rows; i++)
+            $('table').append('<tr></tr>');
+            for (var j = 1; j <= cols; j++) {
+                $('tr:last').append('<td></td>');
+                $('td').attr("class", 'cells');
+            }
+            grid.preventDefault();
+
+            $('.cells').click(function(event) {
+                var paint = $('#colorPicker').val();
+                $(event.target).css('background-color', paint);
+            });
+    });
+});
+
+
+
+
 $('#sizePicker').submit(function (event)  {
     event.preventDefault();
     height = $('#inputHeight').val();
     width = $('#inputWidth').val();
     makeGrid(height, width);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function makeGrid(x,y) {
     $('tr').remove();
